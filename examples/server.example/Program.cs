27849d59a60a330e002ef83b1a7746ad97fd79af
Marcelo Vieira de Souza <dn@dn32.com.br>
2024-01-25 19:08:49 -0300

    Melhorias

--- examples/server.example/Program.cs
+++ examples/server.example/Program.cs
@@ -3,19 +3,28 @@ using server.example.grpc;
 using shared.example.contract;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
 // gRPC configuration
 builder.Services
+        .AddHttpContextAccessor()
         .AddGrpcController<IExampleGrpcController, ExampleGrpcController>()
         .AddGrpcServerDefaultInitialize(builder, 5230, 5231)
         .AddAuthentication(GrpcAuthenticationHandler.SchemeName)
         .AddScheme<GrpcAuthenticationSchemeOptions, GrpcAuthenticationHandler>(GrpcAuthenticationHandler.SchemeName, options => options.AlwaysAuthenticate = true);
 // End gRPC configuration
 
+services.AddAuthorization();
+services.AddAuthentication();
+
 var app = builder.Build();
 
 // gRPC configuration
 app.UseGrpcServerDefaultInitialize();
 // End gRPC configuration
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
--- examples/server.example/grpc/ExampleGrpcController.cs
+++ examples/server.example/grpc/ExampleGrpcController.cs
@@ -1,11 +1,13 @@
-﻿using shared.example.contract;
+﻿using Microsoft.AspNetCore.Authorization;
+using shared.example.contract;
 
 namespace server.example.grpc;
 
-public class ExampleGrpcController : IExampleGrpcController
+public class ExampleGrpcController(IHttpContextAccessor httpContextAccessor) : IExampleGrpcController
 {
     public async Task<GrpcResultContractExampleV1> SimpleExampleAsync(GrpcContractExampleV1 exampleGrpcContract)
     {
+        var user = httpContextAccessor.HttpContext.User;
         await Task.CompletedTask;
         return new GrpcResultContractExampleV1 { Result = $"{exampleGrpcContract.Name}, {string.Join(",", exampleGrpcContract.Metadata?.Select(m => $"{m.Key}: {m.Value}") ?? [])}" };
     }
